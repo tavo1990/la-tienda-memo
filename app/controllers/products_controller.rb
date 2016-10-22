f6f20c4eeb49a3f3603be58f306175e46f780188
mardiniii <sezama03@hotmail.com>
2016-10-22 11:09:36 -0500

    Refactor products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  before_action :find_product, only: [ :edit, :update, :show, :destroy ]
+
   def new
     @product = Product.new
   end
@@ -7,46 +9,45 @@ class ProductsController < ApplicationController
     @product = Product.new(product_params)
     if @product.save
       flash[:notice] = 'The product have been saved'
-      @action = 'created'
-      redirect_to @product
+      redirect_to product_path(id: @product.id, it_was: 'created')
     else
       render :new
     end
   end
 
   def edit
-    @product = Product.find(params[:id])
   end
 
   def update
-    @product = Product.find(params[:id])
     if @product.update(product_params)
       flash[:notice] = 'Store was successfully updated.'
-      @action = 'updated'
-      redirect_to @product
+      redirect_to product_path(id: @product.id, it_was: 'updated')
     else
       render :edit
     end
+  end
+
+  def show
+    @action = params[:it_was]
+  end
+
+  def destroy
+    @product.destroy
+    flash[:notice] = 'Product was successfully destroyed.'
+    redirect_to products_path
   end
 
   def index
     @products = Product.all
   end
 
-  def destroy
-    @product = Product.find(params[:id])
-    @product.destroy
-    flash[:notice] = 'Product was successfully destroyed.'
-    redirect_to products_path
-  end
-
-  def show
-    @product = Product.find(params[:id])
-  end
-
   private
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
       params.require(:product).permit(:name, :reference, :price, :quantity, :brand, :description)
+    end
+
+    def find_product
+      @product = Product.find(params[:id])
     end
 end
