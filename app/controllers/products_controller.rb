67779781de759c5103d332f1b45a9469a728fdfb
mardiniii <sezama03@hotmail.com>
2016-10-22 10:01:10 -0500

    Add product validations

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,15 +3,33 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = 'The product have been saved'
+      redirect_to @product
+    else
+      render :new
+    end
+  end
+
   def edit
   end
 
   def index
+    @products = Product.all
   end
 
   def destroy
   end
 
   def show
+    @product = Product.find(params[:id])
   end
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def product_params
+      params.require(:product).permit(:name, :reference, :price, :quantity, :brand, :description)
+    end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -14,4 +14,8 @@
 #
 
 class Product < ApplicationRecord
+  validates :name, :reference, :price, :quantity, :brand, presence: true
+  validates :price, numericality: true
+  validates :quantity, numericality: { only_integer: true }
+  validates :price, :quantity, numericality: { greater_or_equal_to: 0 }
 end
